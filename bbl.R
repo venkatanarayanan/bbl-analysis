0b652a28bd26462d0db71f46b4701877d4b830d4
venkatanarayanan <venkatanarayanan22@gmail.com>
2020-02-03 12:56:39 +0530

    modified column names

--- bbl.R
+++ bbl.R
@@ -4,6 +4,7 @@ library(stringr)
 library(forcats)
 library(hms)
 library(lubridate)
+library(janitor)
 
 # load the excel and convert the first two rows to a single row
 # bbl_xlsx <- read_excel("<path to the file>")
@@ -18,26 +19,27 @@ library(lubridate)
 bbl_csv <- read.csv("<path to the file>")
 
 # rename the variable names
-names(bbl_csv)[names(bbl_csv) == "Neutral...AlternativeVenue"] <- "Neutral Venue"
-names(bbl_csv)[names(bbl_csv) == "X"] <- "S.no"
+bbl_csv <- clean_names(bbl_csv)
+names(bbl_csv)[names(bbl_csv) == "x"] <- "s_no"
 
 # convert the 'Date' variable to date class
-bbl_csv$Date <- as.Date(bbl_csv$Date, "%d-%m-%Y")
+bbl_csv$date <- as.Date(bbl_csv$date, "%d-%m-%Y")
 
 # convert the 'Time.Local' variable to hms class
-bbl_csv$Time.Local. <- as_hms(parse_date_time(bbl_csv$Time.Local., "IMS p"))
+bbl_csv$time_local <- as_hms(parse_date_time(bbl_csv$time_local, "IMS p"))
 
 # Handle incorrect and missing values
-bbl_csv$Home.Team..H. <- str_replace(bbl_csv$Home.Team..H., "Melborne Renegades",
+bbl_csv$home_team_h <- str_replace(bbl_csv$home_team_h, "Melborne Renegades",
                                      "Melbourne Renegades")
-bbl_csv$Home.Team..H. <- as.factor(bbl_csv$Home.Team..H.)
-bbl_csv$SuperOver. <- fct_explicit_na(bbl_csv$SuperOver., "N")
-bbl_csv$Winner <- str_replace(bbl_csv$Winner, "V", "NR")
-bbl_csv$Winner <- as.factor(bbl_csv$Winner)
-bbl_csv$DLSMethod. <- fct_explicit_na(bbl_csv$DLSMethod., "N")
-bbl_csv$`Neutral Venue`<- fct_explicit_na(bbl_csv$`Neutral Venue`, "N")
-bbl_csv$Notes <- as.character(bbl_csv$Notes)
-bbl_csv$Source <- as.character(bbl_csv$Source)
+bbl_csv$home_team_h <- as.factor(bbl_csv$home_team_h)
+bbl_csv$super_over <- fct_explicit_na(bbl_csv$super_over, "N")
+bbl_csv$winner <- str_replace(bbl_csv$winner, "V", "NR")
+bbl_csv$winner <- as.factor(bbl_csv$winner)
+bbl_csv$dls_method <- fct_explicit_na(bbl_csv$dls_method, "N")
+bbl_csv$neutral_alternative_venue <- fct_explicit_na(bbl_csv$neutral_alternative_venue, 
+                                                     "N")
+bbl_csv$notes <- as.character(bbl_csv$notes)
+bbl_csv$source <- as.character(bbl_csv$source)
 
 # Exclude betting odds values
 bbl_csv_final <- bbl_csv[, c(1:19, 30:33)]
