13cdb664c439c058adcd88bedf9d2772d01da47b
venkatanarayanan <venkatanarayanan22@gmail.com>
2020-02-20 11:08:49 +0530

    code refactoring 1

--- bbl_ball_by_ball.R
+++ bbl_ball_by_ball.R
@@ -55,9 +55,10 @@ bbl_commentary$details <- str_replace(bbl_commentary$details, "OUT", "")
 bbl_commentary <- separate(bbl_commentary, details, c("batsman_name", "runs"), sep = ",")
 bbl_commentary$runs <- str_trim(bbl_commentary$runs)
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("LEG BYE", runs) & is.na(action),
-                                                            "LEG BYE",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("LEG BYE", runs) & is.na(action),
+                         "LEG BYE",
+                         as.character(action)))
 
 bbl_commentary <- bbl_commentary %>%
   mutate(description = ifelse(grepl("RUN OUT", action) & grepl("LEG BYE", runs),
@@ -72,87 +73,87 @@ bbl_commentary <- bbl_commentary %>%
 bbl_commentary$runs <- str_replace(bbl_commentary$runs, "LEG BYE", "")
 bbl_commentary$runs <- str_trim(bbl_commentary$runs)
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("NO BALL", runs),
-                                                            "NO BALL",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("NO BALL", runs),
+                         "NO BALL",
+                         as.character(action)))
+
 bbl_commentary$runs <- str_replace(bbl_commentary$runs, "NO BALL", "")
 bbl_commentary$runs <- str_trim(bbl_commentary$runs)
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("BYE", runs),
-                                                            "BYE",
-                                                            as.character(action)))
+
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("BYE", runs),
+                         "BYE",
+                         as.character(action)))
+
 bbl_commentary$runs <- str_replace(bbl_commentary$runs, "BYE", "")
 bbl_commentary$runs <- str_trim(bbl_commentary$runs)
 
-bbl_commentary <- bbl_commentary %>% mutate(description = ifelse(grepl("WIDE", batsman_name) & !is.na(action),
-                                                                 action,
-                                                                 as.character(description)))
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("WIDE", batsman_name) &
-                                                                   !is.na(action),
-                                                                 "WIDE OUT",
-                                                                 as.character(action)))
-bbl_commentary <- bbl_commentary %>% mutate(batsman_name = ifelse(grepl("WIDE", batsman_name) & !is.na(action),
-                                                str_replace(batsman_name, "WIDE", ""),
-                                                as.character(batsman_name)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(description = ifelse(grepl("WIDE", batsman_name) & !is.na(action),
+                              action,
+                              as.character(description)))
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("[[:digit:]] WIDE", batsman_name),
-                                                            "WIDE",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("WIDE", batsman_name) & !is.na(action),
+                         "WIDE OUT",
+                         as.character(action)))
+
+bbl_commentary <- bbl_commentary %>% 
+  mutate(batsman_name = ifelse(grepl("WIDE", batsman_name) & !is.na(action),str_replace(batsman_name, "WIDE", ""),as.character(batsman_name)))
+
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("[[:digit:]] WIDE", batsman_name),
+                         "WIDE",
+                         as.character(action)))
+
 bbl_commentary <- bbl_commentary %>%
-  mutate(runs = ifelse(grepl("[[:digit:]] WIDES", batsman_name),str_extract(batsman_name, "[[:digit:]]"), as.character(runs)))
+  mutate(runs = ifelse(grepl("[[:digit:]] WIDES", batsman_name),
+                       str_extract(batsman_name, "[[:digit:]]"),
+                       as.character(runs)))
 
-bbl_commentary$batsman_name <- str_replace(bbl_commentary$batsman_name, "[[:digit:]] WIDES", "")
+bbl_commentary$batsman_name <- str_replace(bbl_commentary$batsman_name, 
+                                           "[[:digit:]] WIDES", "")
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("WIDE", batsman_name),
-                                                            "WIDE",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("WIDE", batsman_name), "WIDE",as.character(action)))
+
 bbl_commentary$batsman_name <- str_replace(bbl_commentary$batsman_name, "WIDE", "")
 bbl_commentary$batsman_name <- str_trim(bbl_commentary$batsman_name)
 bbl_commentary$runs <- str_trim(bbl_commentary$runs)
 
-bbl_commentary <- bbl_commentary %>% mutate(description = ifelse(grepl("CATCH", action),
-                                                            action,
-                                                            as.character(description)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(description = ifelse(grepl("CATCH", action),action,as.character(description)))
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("CATCH", action),
-                                                                 "OUT",
-                                                                as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("CATCH", action),"OUT",as.character(action)))
 
-bbl_commentary <- bbl_commentary %>% mutate(description = ifelse(grepl("BOWLED", action),
-                                                                 action,
-                                                                 as.character(description)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(description = ifelse(grepl("BOWLED", action),action,as.character(description)))
 #
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("BOWLED", action),
-                                                            "OUT",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("BOWLED", action),"OUT",as.character(action)))
 
-bbl_commentary <- bbl_commentary %>% mutate(description = ifelse(grepl("LBW", action),
-                                                                 action,
-                                                                 as.character(description)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(description = ifelse(grepl("LBW", action),action,as.character(description)))
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("LBW", action),
-                                                            "OUT",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("LBW", action), "OUT",as.character(action)))
 
-bbl_commentary <- bbl_commentary %>% mutate(description = ifelse(grepl("RUN OUT", action),
-                                                                 action,
-                                                                 as.character(description)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(description = ifelse(grepl("RUN OUT", action),action,as.character(description)))
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("RUN OUT", action),
-                                                            "OUT",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("RUN OUT", action),"OUT",as.character(action)))
 
-bbl_commentary <- bbl_commentary %>% mutate(description = ifelse(grepl("STUMPED", action),
-                                                                 action,
-                                                                 as.character(description)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(description = ifelse(grepl("STUMPED", action),action,as.character(description)))
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("STUMPED", action),
-                                                            "OUT",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("STUMPED", action), "OUT",as.character(action)))
 
-bbl_commentary <- bbl_commentary %>% mutate(description = ifelse(grepl("CTW", action),
-                                                                 action,
-                                                                 as.character(description)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(description = ifelse(grepl("CTW", action),action,as.character(description)))
 
-bbl_commentary <- bbl_commentary %>% mutate(action = ifelse(grepl("CTW", action),
-                                                            "OUT",
-                                                            as.character(action)))
+bbl_commentary <- bbl_commentary %>% 
+  mutate(action = ifelse(grepl("CTW", action),"OUT",as.character(action)))
